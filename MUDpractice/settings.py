e386008a0240fe24ee26da965558894855586787
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2020-01-06 14:11:41 -0800

    typo

--- MUDpractice/settings.py
+++ MUDpractice/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'MUDpractice.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config(default=DATABASE_URL)
+DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'), conn_max_age=600)
 
 
 # Password validation
